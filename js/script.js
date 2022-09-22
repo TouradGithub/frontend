2596452e54b7e333af21ae29e02d8b718731431b
Abdallah-Abdelnour <abdelnourabdallah@gmail.com>
2022-09-21 21:43:52 -0700

    final

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let text;
+
+let btn = document.querySelector('button');
+btn.onclick = function(){
+    let day = document.getElementById('day').value;
+    let manth = document.getElementById('manth').value;
+    let year = document.getElementById('year').value;
+    if(day==""||manth==""||year==""){
+    }else{
+      console.log(writeHijri(new Date(year,manth-1,day), 'ar'));
+    } 
+    document.getElementById('result').innerHTML = text;
+}
+function writeHijri(date, lang) {
+    var date = date || new Date();
+    lang = lang || 'en';
+    var options = {
+      year: 'numeric', month: 'long', day: 'numeric'
+    };
+    text = date.toLocaleString(lang + '-u-ca-islamic', options);
+    return date.toLocaleString(lang + '-u-ca-islamic', options);
+}
+
