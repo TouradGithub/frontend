b3297d13cae847f71f63bf2f903be339c8650e72
zniguefmohamed <mohamedzniguef@gmail.com>
2022-09-25 10:24:12 +0100

    converte img to pdf

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let newImg, displayImg;
+
+function loadImage(event) {
+  displayImg = document.getElementById("displayImg");
+  displayImg.src = URL.createObjectURL(event.target.files[0]);
+
+  newImg = document.createElement("img");
+  newImg.src = URL.createObjectURL(event.target.files[0]);
+
+  displayImg.onload = function () {
+    URL.revokeObjectURL(displayImg.src);
+  };
+}
+
+function downloadPdf() {
+  let doc = new jsPDF();
+  doc.addImage(newImg, 10, 10);
+  doc.save("image_to_pdf.pdf");
+}
