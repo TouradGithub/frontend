c64bf056907d87467a6b3ad3bb29a93f94a5a4b7
abdo389 <atf389@gmail.com>
2022-09-26 13:18:13 +0200

    fix some problem

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let tacses = document.querySelector("#b");
 tacses.oninput = () => {
-  console.log("good");
-  document.querySelector(".result").innerHTML = (
-    (document.querySelector("#a").value / 100) *
-    document.querySelector("#b").value
-  ).toFixed(2);
+  let a = document.querySelector(".result");
+  let b = document.querySelector("#a").value;
+  let c = document.querySelector("#b").value;
+
+  a.innerHTML = b - (b / 100) * c; 
 };
