f29a6ad5d174bbae028493c4663e585b80a34d40
abdo389 <atf389@gmail.com>
2022-10-01 02:57:13 +0200

    (^_^)

--- js/script.js
+++ js/script.js
@@ -1,106 +1,50 @@
-let container = document.querySelector(".container");
-let bullet = document.querySelectorAll(".bullet");
-
-let start = document.querySelector(".start-date");
-let secS = document.querySelector("#sec-S");
-let minitS = document.querySelector("#minit-S");
-let HouerS = document.querySelector("#Hour-S");
-let dayS = document.querySelector("#day-S");
-let monthS = document.querySelector("#manth-S");
-let yearS = document.querySelector("#year-S");
-
-let end = document.querySelector(".end-date");
-let secE = document.querySelector("#sec-E");
-let minitE = document.querySelector("#minit-E");
-let HouerE = document.querySelector("#Hour-E");
-let dayE = document.querySelector("#day-E");
-let monthE = document.querySelector("#manth-E");
-let yearE = document.querySelector("#year-E");
-
-//
-
-//
-
-//
-
-//
-
-//
-
-//
-
-//
-
-//
+let datenow = new Date();
+var options1 = {
+  month: "long",
+};
+var options2 = {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+};
+let higiri = document.querySelector(".higiri");
+let miladi = document.querySelector(".miladi");
+let monthH = document.querySelector(".higiri .month-h");
+let monthM = document.querySelector(".month-m");
+let dayH = document.querySelector(".day span");
+let hh = datenow.toLocaleString("en-u-ca-islamic", options2).split(" ")[0];
+let mm = datenow.toLocaleDateString("en-us", options2);
+let H = document.createTextNode(hh);
+let M = document.createTextNode(mm);
+let day = datenow.toString().split(" ")[0];
 
 window.onload = () => {
-  secS.value || 0;
-  minitS.value || 0;
-  HouerS.value || 0;
-  dayS.value || 0;
-  monthS.value || 0;
-  yearS.value || 0;
+  higiri.appendChild(H);
+  monthH.innerHTML = datenow.toLocaleDateString("ar-sa", options1);
+  miladi.appendChild(M);
+  monthM.innerHTML = datenow.toLocaleDateString("ar-us", options1);
 
-  let dateNow = new Date();
-  yearE.value = dateNow.getFullYear();
-  monthE.value = dateNow.getMonth() + 1;
-  dayE.value = dateNow.getDate();
-  HouerE.value = dateNow.getHours();
-  minitE.value = dateNow.getMinutes();
-  secE.value = dateNow.getSeconds();
-
-  document.querySelector(".year").innerHTML = 0;
-  document.querySelector(".month").innerHTML = 0;
-  document.querySelector(".day").innerHTML = 0;
-  document.querySelector(".houer").innerHTML = 0;
-  document.querySelector(".minit").innerHTML = 0;
-  document.querySelector(".sec").innerHTML = 0;
-};
-
-container.addEventListener("input", () => {
-  let startD = new Date(
-    yearS.value,
-    monthS.value,
-    dayS.value,
-    HouerS.value,
-    minitS.value,
-    secS.value
-  );
-  let endD = new Date(
-    yearE.value,
-    monthE.value,
-    dayE.value,
-    HouerE.value,
-    minitE.value,
-    secE.value
-  );
-  let se = new Date(startD - endD);
-  let ss = +(parseInt(endD.getFullYear()) - parseInt(startD.getFullYear()));
-  document.querySelector(".year").innerHTML = ss;
-  document.querySelector(".month").innerHTML = se.getMonth() + 1;
-  document.querySelector(".day").innerHTML = se.getDate();
-  document.querySelector(".houer").innerHTML = se.getHours();
-  document.querySelector(".minit").innerHTML = se.getMinutes();
-  document.querySelector(".sec").innerHTML = se.getSeconds();
-  console.log(se);
-});
-
-bullet[0].onclick = () => {
-  bullet[0].classList.toggle("active");
-  secS.value = "";
-  minitS.value = "";
-  HouerS.value = "";
-  dayS.value = "";
-  monthS.value = "";
-  yearS.value = "";
-};
-
-bullet[1].onclick = () => {
-  bullet[1].classList.toggle("active");
-  secE.value = "";
-  minitE.value = "";
-  HouerE.value = "";
-  dayE.value = "";
-  monthE.value = "";
-  yearE.value = "";
+  switch (day) {
+    case "Mon":
+      dayH.innerHTML = "الاثنين";
+      break;
+    case "Tue":
+      dayH.innerHTML = "الثلاثاء";
+      break;
+    case "Wed":
+      dayH.innerHTML = "الأربعاء";
+      break;
+    case "Thu":
+      dayH.innerHTML = "الخميس";
+      break;
+    case "Fri":
+      dayH.innerHTML = "الجمعة";
+      break;
+    case "Sat":
+      dayH.innerHTML = "السبت";
+      break;
+    case "Sun":
+      dayH.innerHTML = "الأحد";
+      break;
+  }
 };
