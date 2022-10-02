d6fedb74b4f1ad4a2058af80a40f77f84eb37e07
abdo389 <atf389@gmail.com>
2022-10-02 18:06:42 +0200

    (:

--- script.js
+++
@@ -1,8 +0,0 @@
-let tacses = document.querySelector("#b");
-tacses.oninput = () => {
-  console.log("good");
-  document.querySelector(".result").innerHTML = (
-    (document.querySelector("#a").value / 100) *
-    document.querySelector("#b").value
-  ).toFixed(2);
-};
